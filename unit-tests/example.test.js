4c56c7684bc0349b411530c942b54a9174105e85
Rishabh Tatia <tatiaris2001@gmail.com>
2024-04-26 03:07:15 -0700

    husky

--- unit-tests/example.test.js
+++ unit-tests/example.test.js
@@ -1,6 +1,7 @@
-import { sum } from '../src/components/helper';
+const sum = (a, b) => {
+  return a + b;
+};
 
 test('adds 1 + 2 to equal 3', () => {
-  // expect(sum(1, 2)).toBe(3);
-  expect(sum(1, 2)).toMatchSnapshot();
+  expect(sum(1, 2)).toBe(3);
 });

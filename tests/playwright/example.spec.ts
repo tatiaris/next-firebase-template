af22db33af80f2ba3c75611a70d7c7a6fc618e55
tatiaris <tatiaris2001@gmail.com>
2024-10-20 06:30:11 -0700

    playwright

--- tests/playwright/example.spec.ts
+++ tests/playwright/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('example', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page.getByText(/home/)).toBeVisible();
   // await page.locator('[automation-id="btn-log-in"]').click();

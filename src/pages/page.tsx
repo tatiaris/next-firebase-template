e67f2f964ed0901bbfd07b5acb70765e8b225c10
tatiaris <tatiaris2001@gmail.com>
2024-10-31 01:35:30 -0500

    fix

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -3,6 +3,7 @@ import LoginForm from "@components/forms/login";
 import NoteForm from "@components/forms/note";
 import Loading, { LoadingComponent } from "@components/ui/loading";
 import { useAuth } from "@hooks/useAuth";
+import { FORM_TYPE } from "@components/forms/utils";
 
 export default function Home() {
   const { user, isGuest, isLoading } = useAuth();
@@ -17,7 +18,7 @@ export default function Home() {
 
   return !isGuest && user ? (
     <div className="py-8 px-8">
-      <NoteForm />
+      <NoteForm formType={FORM_TYPE.CREATE} />
       <br /><br />
       <RecentNotes />
     </div>
